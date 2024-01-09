e69b08dc553fee62394e48741a7c9b3f2b6e0404
isohamhalbandge <logzii123@gmail.com>
2024-01-09 07:36:14 +0530

    02 completed

---
+++ code/02/forLoop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // to print coder army 5 times
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Coder Army" << endl;
+    }
+}
